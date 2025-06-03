7e9d5f8abe0b4e2ac04251248f2a379e52a608fa
valley <cksal0805@kakao.com>
2025-06-03 21:09:42 +0900

    feat: import

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,10 @@
-import { HomeNav } from '../../components/ui/home-nav';
-import styles from './page.module.css';
-import { TodayNewsSection } from '../../components/today-news/today-news-section';
-import { Spacing } from '../../components/ui/spacing';
-import { HotNews } from '@/components/today-news/hot-news';
-import { BottomNavigateBar } from '@/components/ui/bottom-navigate-bar';
 import { Keywords } from '@/components/keywords';
+import { HomeNav } from '../../components/ui/home-nav';
+import styles from '../page.module.css';
+import { TodayNewsSection } from '@/components/today-news/today-news-section';
+import { HotNews } from '@/components/today-news/hot-news';
+import { Spacing } from '@/components/ui/spacing';
+import { BottomNavigateBar } from '@/components/ui/bottom-navigate-bar';
 
 export default function Home() {
   return (
